86d1e8577b1cb18cd0134b92e5e62754b87b8c17
Fejes Aniko <oranjje@gmail.com>
2016-08-11 19:18:36 +0200

    add fillfox function

--- canvas-base.js
+++ canvas-base.js
@@ -6,3 +6,20 @@ var canvas = canvasElement.getContext('2d');
 function rgb(r, g, b) {
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+function rgba(r, g, b, a) {
+  return `rgb(${r}, ${g}, ${b}, ${a})`;
+}
+
+canvas.fillFox = function (x, y, s) {
+  this.beginPath();
+  this.moveTo(x, y+s/7*3);
+  this.lineTo(x+s/7*2, y);
+  this.lineTo(x+s/7*3, y+s/7*2);
+  this.lineTo(x+s/7*4, y+s/7*2);
+  this.lineTo(x+s/7*5, y);
+  this.lineTo(x+s, y+s/7*3);
+  this.lineTo(x+s/2, y+s);
+  this.lineTo(x, y+s/7*3);
+  this.fill();
+}
