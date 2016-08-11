bbdf1223388e9a10ffb363d4cc302a2a6fa54693
Fejes Aniko <oranjje@gmail.com>
2016-08-11 20:56:15 +0200

    add onWave method

--- canvas-base.js
+++ canvas-base.js
@@ -23,3 +23,22 @@ canvas.fillFox = function (x, y, s) {
   this.lineTo(x, y+s/7*3);
   this.fill();
 }
+
+// Initialize Firebase
+const config = {
+  // I think I shouldn't leave it here
+  apiKey: "AIzaSyBd4qBMVtu1tO2bXdNHoJq3UbY0FbPN-7c",
+  // nevermind
+  authDomain: "pebble-wave.firebaseapp.com",
+  databaseURL: "https://pebble-wave.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
+
+canvas.onWave = function (callback) {
+  let fbMoving = firebase.database().ref('moving');
+  fbMoving.on('value', function(dataSnapshot) {
+   let moving = dataSnapshot.val() === 'true';
+   callback(moving);
+  });
+}
